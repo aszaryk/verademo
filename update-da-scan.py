a09aee73367436142ff39325ba0daaaa32e0736b
Andrzej Szaryk <aszaryk@veracode.com>
2020-02-03 17:03:24 -0500

    debug

--- update-da-scan.py
+++ update-da-scan.py
@@ -96,11 +96,11 @@ except:
     sys.exit(1)
 
 #Update Schedule of existing DA Job
-res = prepared_request('PUT', 'https://api.veracode.com/was/configservice/v1/analyses/' + job_id + '?method=PATCH', json=data)
+res1 = prepared_request('PUT', 'https://api.veracode.com/was/configservice/v1/analyses/' + job_id + '?method=PATCH', json=data)
 if res.status_code == 204:
     print("Scan Submitted Successfully: " + str(res.status_code) )
 else:
-    response = res.json()
+    response = res1.json()
     print("Error encountered: " + response['_embedded']['errors'][0]['detail'])
 
 
