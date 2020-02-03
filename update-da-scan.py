28352abac01bd12bc8f66de4818b4b5307877cdc
Andrzej Szaryk <aszaryk@veracode.com>
2020-02-03 16:41:49 -0500

    debugging

--- update-da-scan.py
+++ update-da-scan.py
@@ -89,6 +89,7 @@ print("Looking for Dynamic Analysis Job: " + dynamic_job )
 res = prepared_request('GET', 'https://api.veracode.com/was/configservice/v1/analyses', query=("name=" + dynamic_job))
 response = res.json()
 try:
+    print response
     job_id = response['_embedded']['analyses'][0]['analysis_id']
     #Update Schedule of existing DA Job
     res = prepared_request('PUT', 'https://api.veracode.com/was/configservice/v1/analyses/' + job_id + '?method=PATCH', json=data)
